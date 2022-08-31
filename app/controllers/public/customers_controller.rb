242424f607ffc5780e01609a05bb23b092b7c622
EC2 Default User <ec2-user@ip-172-31-41-145.ap-northeast-1.compute.internal>
2022-08-31 10:56:35 +0000

    [fix] 顧客情報変更

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -11,7 +11,7 @@ class Public::CustomersController < ApplicationController
   def update
      @customer = current_customer
      if @customer.update(customer_params)
-       redirect_to public_customer_path(@customer.id)
+       redirect_to public_homes_top_path
        flash[:notice] = "ご登録情報の変更が終了しました"
      else
        flash[:notice] = "変更が失敗しました"
